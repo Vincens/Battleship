3c945d57e68953f6a0098c0dc72b7adf18e8655b
PBialek1 <peterbialek1@gmail.com>
2015-02-12 11:47:55 -0500

    Update Battlefield.java

--- Battlefield.java
+++ Battlefield.java
@@ -3,19 +3,93 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Grid here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author PeterB BenL 
+ * @version 1.0
  */
 public class Battlefield extends World
 {
 
-    /**
-     * Constructor for objects of class Grid.
-     * 
-     */
-    public Battlefield()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 800, 1); 
+//constructor
+public Battlefield()
+{    
+        super(1008, 1010, 1); 
+}
+
+//firing/hit registration  
+public void attack(){
+    String firingcode = greenfoot.ask("Input firing code");
+    int Ycode = parseInt(firingcode.substring(1)) - 1;
+    int Xcode = charToInt(firingcode.substring(0,1));
+    checkHit(Xcode, Ycode);
+}
+
+//checks coordinates and returns what is at the location
+public void checkHit(int x, int y){
+    
+}
+
+//converts char to int
+public int charToInt(String input){
+    int output = 0;
+    boolean flag = false;
+    while (!flag){
+        switch(input.toLowerCase()) {
+            case "a":
+                output = 0;
+                flag = true;
+                break;
+            case "b":
+                output = 1;
+                flag = true;
+                break;
+            case "c": 
+                output = 2;
+                flag = true;
+                break;
+            case "d": 
+                output = 3;
+                flag = true;
+                break;
+            case "e": 
+                output = 4;
+                flag = true;
+                break;
+            case "f": 
+                output = 5;
+                flag = true;
+                break;
+            case "g": 
+                output = 6;
+                flag = true;
+                break;
+            case "h": 
+                output = 7;
+                flag = true;
+                break;
+            case "i": 
+                output = 8;
+                flag = true;
+                break;
+            case "j": 
+                output = 9;
+                flag = true;
+                break;
+            default: 
+                flag = false;
+                break;
+        }
     }
+return output;
+}
+
+/* values for char array:
+    e empty
+    h hit
+    m miss
+    c carrier
+    b battleship
+    d destroyer
+    s submarine
+    p patrol boat
+*/   
 }
