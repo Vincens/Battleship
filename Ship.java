eee9a50994859395f6423ae5e6970b8a055e6a21
PBialek1 <peterbialek1@gmail.com>
2015-02-19 15:18:48 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Ship here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @PeterB BenL
+ * @1.0
  */
 public class Ship extends Actor
 {
