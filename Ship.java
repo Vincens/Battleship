f29385c850e18d51d882729884a2090cc13331b2
PBialek1 <peterbialek1@gmail.com>
2015-02-19 15:17:46 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -9,16 +9,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Ship extends Actor
 {
     private int shipHealth;
+    private int owner;
     public Ship (int health)
     {
         this.shipHealth = health;
+        this.owner = 1;
+    }
+    public Ship (int health, int player)
+    {
+        this.shipHealth = health;
+        this.owner = player;
+    }
     }
     public void hit()
     {
         this.shipHealth--;
+        checkSunk();
     }
     public int getHealth()
     {
         return this.shipHealth;
+    }
+    private boolean checkSunk()
+    {
+        if (this.shipHealth == 0)
+        
+    }
     }
 }
