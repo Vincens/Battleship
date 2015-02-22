232677c7366e48c896c12c49ce332609b7f6d864
PBialek1 <peterbialek1@gmail.com>
2015-02-22 14:11:02 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -9,20 +9,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  
 public class Ship extends Actor
 {
-    private int shipHealth;
+    private int shipHealth; // i.e. length of ship
     private int owner;
     private static String endpt1;
-    public Ship (int health)
+    private char type; //will fill the array at ship's location
+    public Ship (int health, char shiptype)
     {
         this.shipHealth = health;
         this.owner = 1;
+        this.type = shiptype;
         GreenfootImage Ship = new GreenfootImage("ShipPlaceholder.png");
         this.setImage(Ship);
     }
     
-    public Ship (int health, int player)
+    public Ship (int health, int player, char shiptype)
     {
         this.shipHealth = health;
+        this.type = shiptype;
         this.owner = player;
         GreenfootImage Ship = new GreenfootImage("ShipPlaceholder.png");
         this.setImage(Ship);
@@ -33,6 +36,24 @@ public class Ship extends Actor
         endpt1 = Greenfoot.ask("Input coordinate: ");
         int Ycode = Integer.parseInt(endpt1.substring(1)) - 1;
         int Xcode = Battlefield.charToInt(endpt1);
+        // ____________ need to create arrow image at endpt1
+        int orientation = 0;
+        // 0=up 1=right 2=down 3=left
+        //rotate left and right by 90 degrees until position is set with enter
+        while (!Greenfoot.isKeyDown("enter")){
+            if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")){
+                orientation--;
+                if (orientation < 0)
+                    orientation = 3;
+                arrow.turn(-90);
+            }
+            if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")){
+                orientation++;
+                if (orientation > 3)
+                    orientation = 0;
+                arrow.turn(90);
+            }
+        }
         
     }
     
