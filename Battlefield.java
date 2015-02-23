4c2b3d056025df7eeb5b6caac87896a0c2332511
Ben Lindstrom <lindstrom.ben@gmail.com>
2015-02-22 20:32:28 -0500

    Lots of fixes

--- Battlefield.java
+++ Battlefield.java
@@ -12,6 +12,11 @@ private static char[][] grid1;
 private static  char[][] grid2;
 private static int p1Score;
 private static int p2Score;
+Ship Submarine = new Ship(3, 's');
+Ship Patrol = new Ship(2, 'p');
+Ship Battleship = new Ship(4, 'b');  
+Ship Carrier = new Ship(5, 'c');
+Ship Destroyer = new Ship(3, 'd');
 //constructor
 public Battlefield()
 {    
